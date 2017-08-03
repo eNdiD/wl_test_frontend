493bcaea344e24f9900d73baa42e4ddf45d95989
endid <endid13@gmail.com>
2017-08-03 15:29:07 +0300

    Changed API url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const ROOT_URL = 'http://127.0.0.1:8000/';
+// export const ROOT_URL = 'http://127.0.0.1:8000/';
+export const ROOT_URL = 'http://wl-test-api.omega.in.ua/';
 export const API_ROOT_URL = `${ ROOT_URL }api/v0/`;
